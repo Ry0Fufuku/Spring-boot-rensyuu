b68eece7e61d5ee768d208e87ecfbcc725346ce3
oyama <oyama@192.168.10.128>
2025-07-15 15:52:17 +0900

    07/15,15:52--サイトが表示されませんでした

--- trainingspring/src/main/java/com/example/demo/controller/UserController.java
+++ trainingspring/src/main/java/com/example/demo/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
+
 
 /*
  * ユーザー情報 Controller
@@ -22,13 +27,19 @@ public class UserController {
 	/*
 	 * ユーザー情報一覧画面を表示
 	 * @param model Model
-	 * @return ユーザー情報一覧画面
+	 * @return ユーザー情報一覧画面のHTML
 	 */
-	@GetMapping(value = "/user/list")
+	@RequestMapping(value = "/user/list", method = RequestMethod.GET)
 	public String displayList(Model model) {
 		List<User> userlist = userService.searchAll();
 		model.addAttribute("userlist",userlist);
 		return "user/list";
+	
+//	@GetMapping(value = "/user/list")
+//	public String displayList(Model model) {
+//		List<User> userlist = userService.searchAll();
+//		model.addAttribute("userlist",userlist);
+//		return "user/list";
 	}
 	
 	/*
