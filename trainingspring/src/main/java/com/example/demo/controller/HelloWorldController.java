f4d9c170e031922655aae547f37146696829f278
oyama <oyama@192.168.10.128>
2025-07-15 16:03:29 +0900

    Whitelabel Error Page

--- trainingspring/src/main/java/com/example/demo/controller/HelloWorldController.java
+++ trainingspring/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,11 +1,10 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
+//@Controller
 public class HelloWorldController {
 	
 	@RequestMapping(value = "/",method = RequestMethod.GET)
