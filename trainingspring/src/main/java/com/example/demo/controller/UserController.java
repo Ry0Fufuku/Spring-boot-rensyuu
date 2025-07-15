6b7464a0c515b94805fc542f1941afdf599e2a2e
oyama <oyama@192.168.10.128>
2025-07-15 13:03:47 +0900

    2025/07/15,13:02-Spring Boot + Thymeleafで一覧画面を作成する、コントローラー作成途中

---
+++ trainingspring/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.service.UserService;
+
+/*
+ * ユーザー情報 Controller
+ */
+@Controller
+public class UserController {
+	
+	/*
+	 * ユーザー情報 Service
+	 */
+	@Autowired
+	private UserService userService;
+	
+	/*
+	 * ユーザー情報一覧画面を表示
+	 * @param model Model
+	 * @return ユーザー情報一覧画面
+	 */
+	@GetMapping(value = "/user/list")
+	public String displayList(Model model) {
+		List<User> userlist = userService.searchAll();
+		model.addAttribute("userlist",userlist);
+		return "user/list";
+	}
+	
+	/*
+	 * ユーザー新規登録画面を表示
+	 */
+}
